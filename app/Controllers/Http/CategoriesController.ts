7fba674b28c97fc13d8b3fce9c6082db4b877497
An <an.tran511@outlook.com>
2023-10-11 16:47:53 +0700

    fix: refactor categories controller

--- app/Controllers/Http/CategoriesController.ts
+++ app/Controllers/Http/CategoriesController.ts
@@ -2,7 +2,7 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Category from 'App/Models/Category'
 
 export default class CategoriesController {
-  public async index({}: HttpContextContract) {
+  public async index({ response }: HttpContextContract) {
     const categories = await Category.query().preload('parentCategory', (query) => {
       query.preload('parentCategory', (query) => {
         query.preload('parentCategory', (query) => {
@@ -12,6 +12,6 @@ export default class CategoriesController {
         })
       })
     })
-    return categories
+    return response.send({ data: categories })
   }
 }
