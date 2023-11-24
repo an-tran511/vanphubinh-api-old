7676614c369b074ef83293f5cd2c338baa0e1a29
an-tran511 <an.tran511@outlook.com>
2023-11-24 10:42:49 +0700

    feat: update locations query

--- app/Controllers/Http/LocationsController.ts
+++ app/Controllers/Http/LocationsController.ts
@@ -1,9 +1,31 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Location from 'App/Models/Location'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 export default class LocationsController {
   public async index({ response }: HttpContextContract) {
-    const locations = await Location.query()
-    return response.ok(locations)
+    const locations = await Database.rawQuery(
+      `WITH RECURSIVE locations_cte(id, name, parent, ancestors) AS (
+          SELECT
+            locations.id,
+            locations.name,
+            locations.parent_id,
+            ARRAY [locations.name]
+              FROM locations
+              WHERE locations.parent_id IS NULL
+          UNION ALL
+          SELECT
+            locations.id,
+            locations.name,
+            locations.parent_id,
+            array_append(locations_cte.ancestors, locations.name)
+              FROM locations_cte,
+                  locations
+              WHERE locations.parent_id = locations_cte.id
+      )
+      SELECT locations_cte.id, array_to_string(locations_cte.ancestors, ' / ') as name, locations.location_type as "locationType"
+      FROM locations_cte JOIN locations ON locations.id = locations_cte.id;`
+    )
+
+    return response.ok(locations?.rows)
   }
 }
